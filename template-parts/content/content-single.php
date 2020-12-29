4cb9eeeafb49ac10255572b69a262c068973e4a2
Kev <kev@quirk.cc>
2020-12-29 16:58:58 +0000

    Fixed icons

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -44,7 +44,7 @@
 
 	<p>I don't have comments on this site as they're difficult to manage and take up too much time. I'd rather concentrate on producing content than managing comments.</p>
 
-	<p>Instead of leaving a comment, you could 📝 <a href="/guestbook">sign my guestbook</a> or ✉️ <a href="/contact">contact me</a> instead.
+	<p>Instead of leaving a comment, you could <a href="/guestbook">📝 sign my guestbook</a> or <a href="/contact">✉️ contact me</a> instead.
 
 	<?php
 
