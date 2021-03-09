5b84267c25a7c51bd6b8b02a994d779848f9bb47
Kev <kev@quirk.cc>
2021-03-09 09:07:41 +0000

    css

--- functions.php
+++ functions.php
@@ -1,14 +1,18 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-    $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-    $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css',
-        array(),  // if the parent theme code has a dependency, copy it to here
-        $theme->parent()->get('Version')
-    );
+
+// Load the 2021 stylesheet
+add_action( 'wp_enqueue_scripts', '2021_styles' );
+function 2021_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( $parenthandle ),
-        $theme->get('Version') // this only works if you have Version in the style header
+        array( 'parenthandle' ),
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
     );
 }
+
+// Load the theme stylesheets
+function theme_styles()
+{
+	// Load my CSS
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css' );
+}
+add_action('wp_enqueue_scripts', 'theme_styles');
