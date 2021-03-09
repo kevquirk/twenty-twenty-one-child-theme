b6395f128831cf735d248673972ceb2cafb2e0bc
Kev <kev@quirk.cc>
2021-03-09 09:16:30 +0000

    dfg

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
 
 // Load the theme stylesheets
-function theme_styles()
-{
-	// Load my CSS
-	wp_enqueue_style( '/wp-content/themes/twentytwentyone-child/style.css' );
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'jobcareertheme';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('1.0.0')
+    );
 }
-add_action('wp_enqueue_scripts', 'theme_styles');
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
