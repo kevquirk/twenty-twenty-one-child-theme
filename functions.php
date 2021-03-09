f5c2ecdd8b4e28cd7ea3ca58fdfa1a60c47cea07
Kev <kev@quirk.cc>
2021-03-09 09:11:19 +0000

    cbncn

--- functions.php
+++ functions.php
@@ -4,6 +4,6 @@
 function theme_styles()
 {
 	// Load my CSS
-	wp_enqueue_style( 'main', '/wp-content/themes/twentytwentyone-child/style.css' );
+	wp_enqueue_style( '/wp-content/themes/twentytwentyone-child/style.css' );
 }
 add_action('wp_enqueue_scripts', 'theme_styles');
