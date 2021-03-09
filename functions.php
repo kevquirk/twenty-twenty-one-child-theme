f3faeecd38efbabf8b763cf43941efb7e95b62f0
Kev <kev@quirk.cc>
2021-03-09 09:17:32 +0000

    fghj

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 // Load the theme stylesheets
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'jobcareertheme';
+    $parent_style = 'twentytwentyone';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
