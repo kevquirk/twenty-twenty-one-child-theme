76a889070f72f625eb46e8a91d399a9917f3a1ac
Kev <kev@quirk.cc>
2021-03-09 08:49:58 +0000

    Stylesheet fixed (I hope)

--- functions.php
+++ functions.php
@@ -7,11 +7,11 @@ function tutsplus_parent_styles() {
 
 }
 
-/* Enqueues the external CSS file */
-add_action( 'wp_enqueue_scripts', 'tutsplus_external_styles' );
-function tutsplus_external_styles() {
+/* Add custom stylesheet */
+function additional_stylesheets() {
 
-    wp_register_style( 'child-css', get_stylesheet_directory_uri().'/child.css' );
-    wp_enqueue_style( 'child-css' );
-
+wp_register_style( 'child', get_template_directory_uri().'/child.css'  );
+wp_enqueue_style( 'child' );
 }
+
+add_action( 'wp_enqueue_scripts', 'additional_stylesheets' );
