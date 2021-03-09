1cfa051794ff72420767c1ffffe2fea4d93245b8
Kev <kev@quirk.cc>
2021-03-09 08:57:21 +0000

    Child stylesheet?

--- functions.php
+++ functions.php
@@ -1,17 +1,13 @@
 <?php
-/* Enqueues the child theme - Required for the child theme */
-add_action( 'wp_enqueue_scripts', 'tutsplus_parent_styles');
-function tutsplus_parent_styles() {
-
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-
+add_action( 'wp_enqueue_scripts', '2021_styles' );
+function 2021_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( 'parenthandle' ),
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
 }
 
-/* Add custom stylesheet */
-function additional_stylesheets() {
-
-wp_register_style( 'child', get_template_directory_uri().'/child.css'  );
-wp_enqueue_style( 'child' );
+add_action( 'wp_enqueue_scripts', 'child_stylesheet' );
+function child_stylesheet() {
+    wp_enqueue_style( 'my-style', get_stylesheet_directory_uri() . '/child.css', false, '1.0', 'all' );
 }
-
-add_action( 'wp_enqueue_scripts', 'additional_stylesheets' );
