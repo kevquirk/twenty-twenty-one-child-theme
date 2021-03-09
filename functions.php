5d9be7cd0ad5201d01626e8263c90c93dfbb3b45
Kev <kev@quirk.cc>
2021-03-09 09:22:24 +0000

    Does this finally work??

--- functions.php
+++ functions.php
@@ -1,15 +1,8 @@
 <?php
+/* enqueue scripts and style from parent theme */
 
-// Load the theme stylesheets
-function my_theme_enqueue_styles() {
-
-    $parent_style = 'twentytwentyone';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('1.0.0')
-    );
+function twentytwentyone_styles() {
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+	array( 'twenty-twenty-one-style' ), wp_get_theme()->get('Version') );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
