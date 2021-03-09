654a26dade779a9d760d3e7b15d4d688ad952c97
Kev <kev@quirk.cc>
2021-03-09 09:09:30 +0000

    vb

--- functions.php
+++ functions.php
@@ -1,14 +1,5 @@
 <?php
 
-// Load the 2021 stylesheet
-add_action( 'wp_enqueue_scripts', '2021_styles' );
-function 2021_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( 'parenthandle' ),
-        wp_get_theme()->get('Version') // this only works if you have Version in the style header
-    );
-}
-
 // Load the theme stylesheets
 function theme_styles()
 {
