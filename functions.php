067b91ad8fbb7df70208b8ebe5161fb2f2d2f801
Kev <kev@quirk.cc>
2021-03-11 09:12:22 +0000

    Added editor theme

--- functions.php
+++ functions.php
@@ -6,3 +6,7 @@ function twentytwentyone_styles() {
 	array( 'twenty-twenty-one-style' ), wp_get_theme()->get('Version') );
 }
 add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
+
+// Gutenberg custom stylesheet
+add_theme_support('editor-styles');
+add_editor_style( 'editor-style.css' ); // make sure path reflects where the file is located
